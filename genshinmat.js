464d8a0fa801484e9d3daefbb1a726009a924b28
Keegan LePage <keeganlepage@dyn-10-140-1-185.d.umn.edu>
2021-11-23 15:51:39 -0600

    Created the skeleton and format for the material list

--- genshinmat.js
+++ genshinmat.js
@@ -7,19 +7,20 @@ function charDropdown() {
 }
 
 function getCharMats(charName) {
-    document.getElementById("pasteChar").innerHTML = charName;
+    document.getElementById("charName").innerHTML = charName;
+    document.getElementById("charMats").style.display = "";
 }
   
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
